8f27bcda6e000376d2478eb4aa20abfb9e72c471
Erik Bosman <erik@minemu.org>
2020-12-25 02:20:39 +0100

    color component rotation animations

---
+++ xmaslights-gradient.py
@@ -0,0 +1,114 @@
+
+# I'm guessing this is no problem either :-P
+import cmath
+
+
+def xmaslight():
+    # This is the code from my 
+    
+    #NOTE THE LEDS ARE GRB COLOUR (NOT RGB)
+    
+    # Here are the libraries I am currently using:
+    import time
+    import board
+    import neopixel
+    import re
+    import math
+    
+    # You are welcome to add any of these:
+    # import random
+    # import numpy
+    # import scipy
+    # import sys
+    
+    # If you want to have user changable values, they need to be entered from the command line
+    # so import sys sys and use sys.argv[0] etc
+    # some_value = int(sys.argv[0])
+    
+    # IMPORT THE COORDINATES (please don't break this bit)
+    
+    coordfilename = "Python/coords.txt"
+    
+    fin = open(coordfilename,'r')
+    coords_raw = fin.readlines()
+    
+    coords_bits = [i.split(",") for i in coords_raw]
+    
+    coords = []
+    
+    for slab in coords_bits:
+        new_coord = []
+        for i in slab:
+            new_coord.append(int(re.sub(r'[^-\d]','', i)))
+        coords.append(new_coord)
+    
+    #set up the pixels (AKA 'LEDs')
+    PIXEL_COUNT = len(coords) # this should be 500
+    
+    pixels = neopixel.NeoPixel(board.D18, PIXEL_COUNT, auto_write=False)
+    
+    
+    # YOU CAN EDIT FROM HERE DOWN
+
+    # pause between cycles (normally zero as it is already quite slow)
+    slow = 0
+    
+    # scale down to within unit sphere
+    coords = [ (x/500., y/500., z/500.) for x,y,z in coords ]
+
+    gamma = 2.2
+    factor = 255 / (255**gamma)
+    gamma_map = [ int( (x**gamma * factor + .5) ) for x in range(256) ]
+
+    def led_color(color):
+        r, g, b = color
+        r = max(0, min(255, int(r)))
+        g = max(0, min(255, int(g)))
+        b = max(0, min(255, int(b)))
+        return [gamma_map[g],gamma_map[r],gamma_map[b]]
+
+    def dist_sq(a,b):
+        ax,ay,az=a
+        bx,by,bz=b
+        dx,dy,dz=ax-bx,ay-by,az-bz
+        return dx*dx+dy*dy+dz*dz
+
+    def phase(a,b):
+        return (cmath.phase(a+1j*b)/math.pi/2 * 2048) % 2048
+
+    class GradientAni:
+
+        def __init__(self, coords):
+            self.coords = [ (phase(y,z), phase(z,x), phase(x,y)) for x,y,z in coords ]
+            self.buf = [ [0.,0.,0.] for x in range(len(coords)) ]
+            self.rotation = [0,0,0]
+            self.wave = [ int(((1+math.sin(i/2048.*2*math.pi))/2)**2 * 255) for i in range(2048) ]
+
+        def clear(self):
+            for c in self.buf:
+                c[0] = c[1] = c[2] = 0.
+
+        def next(self):
+            self.clear()
+            ri, rj, rk = self.rotation
+            self.rotation = (ri+15)%2048, (rj+13)%2048, (rk+19)%2048
+            for i, rot in enumerate(self.coords):
+                di, dj, dk = rot
+                self.buf[i][1]=self.wave[int(ri+di)%2048]
+                self.buf[i][0]=self.wave[int(rj+dj)%2048]
+                self.buf[i][2]=self.wave[int(rk+dk)%2048]
+
+    ani = GradientAni(coords)
+
+    while True:
+        time.sleep(slow)
+        ani.next()
+        for i, color in enumerate(ani.buf):
+            pixels[i] = led_color(color)
+        pixels.show()
+        
+    return 'DONE'
+
+
+# yes, I just put this at the bottom so it auto runs
+xmaslight()
